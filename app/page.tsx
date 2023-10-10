c93f2cf52a4529e57ffb95e85731905657220d56
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-10-10 23:45:14 +0000

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import  Banner  from "./components/Banner";
 import Header from "./components/Header";
+import Courses from "./components/Courses";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Header />
       <Banner />
+      <Courses />
     </div>
 
   )
