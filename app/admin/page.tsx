37fc0f32afb35f52a05cd2c6ff54f05eb78146f3
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-07 23:10:09 +0000

    admin00

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function Admin(){
+    return(
+        <div>
+        <h1 className="text-center text-8xl font-bold  border-b border-[#F56A6A] px-3 py-[3px] py-16">Área administrativa</h1>
+        <a href="/admin/course">Listar Curso</a>
+        </div>
+    )
+}
