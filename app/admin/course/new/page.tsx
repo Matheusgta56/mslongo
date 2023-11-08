65360edc7c6ccf3d7c4efaaed12e3fbd2387be66
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-08 01:16:50 +0000

    admin002

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,12 +1,16 @@
 export default function NewCourse() {
+    async function saveCourses() {
+        "use server"
+        console.log("acessou a função")
+    }
     return (
         <div>
             <h1>Cadastrar Curso</h1>
             <form>
-                <input  type="text" placeholder="Digite o Título do Curso"/>
-                <input  type="text" placeholder="Digite a Descrição do Curso"/>
-                <input  type="text" placeholder="Digite a URL do Curso"/>
-                <button>Salvar</button>
+                <input  type="text" name="title" placeholder="Digite o Título do Curso"/>
+                <input  type="text" name="descritpion" placeholder="Digite a Descrição do Curso"/>
+                <input  type="text" name="url" placeholder="Digite a URL do Curso"/>
+                <button formAction={saveCourses}>Salvar</button>
             </form>
         </div>
     )
