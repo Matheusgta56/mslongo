3a173e56de16b3d7e58b43976b68d6cda35481b4
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-08 01:19:32 +0000

    admin003

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,7 @@ export default function Admin(){
         <div>
         <h1 className="text-center text-8xl font-bold  border-b border-[#F56A6A] px-3 py-[3px] py-16">Área administrativa</h1>
         <a href="/admin/course" className="text-[#F56A6A] text-xl text-center bg-white border-2 border-[#F56A6A] px-3 py-[3px] py-8">Listar Curso</a>
-        <a href="/admin/course" className="text-[#F56A6A] text-xl text-center bg-white border-2 border-[#F56A6A] px-5 py-[3px] py-8"> Adicionar Curso </a>
+        <a href="/admin/course/new" className="text-[#F56A6A] text-xl text-center bg-white border-2 border-[#F56A6A] px-5 py-[3px] py-8"> Adicionar Curso </a>
         </div>
     )
 }
