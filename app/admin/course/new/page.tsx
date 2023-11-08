3c66f1229291ec447ba49d2db2d53583fc66a46d
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-08 01:28:02 +0000

    admin005

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -5,12 +5,12 @@ export default function NewCourse() {
     }
     return (
         <div>
-            <h1>Cadastrar Curso</h1>
+            <h1 className="text-xl border-b py-8">Cadastrar Curso</h1>
             <form>
                 <input  type="text" name="title" placeholder="Digite o Título do Curso"/><br></br>
                 <input  type="text" name="descritpion" placeholder="Digite a Descrição do Curso"/><br></br>
-                <input  type="text" name="url" placeholder="Digite a URL do Curso"/><br></br>
-                <button formAction={saveCourses}>Salvar</button>
+                <input  className="py-4" type="text" name="url" placeholder="Digite a URL do Curso"/><br></br>
+                <button formAction={saveCourses} className="text-[#F56A6A] text-xl text-center bg-white border-2 border-[#F56A6A] px-5 py-[3px] py-8">Salvar</button>
             </form>
         </div>
     )
