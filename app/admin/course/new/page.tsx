b8de9cfea324fff6eb841b0cad7285896661f142
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-21 22:36:23 +0000

    admin2

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -1,17 +1,26 @@
+import { sql } from "@vercel/postgres";
+
+export const revalidate =0
+
 export default function NewCourse() {
-    async function saveCourses() {
+    async function saveCourse(formData: FormData){
         "use server"
-        console.log("acessou a função")
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
+        const url = formData.get("url") as string;
+        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${url})`
+        console.log("Acessou a função")
     }
     return (
         <div>
-            <h1 className="text-xl border-b py-8">Cadastrar Curso</h1>
+            <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
             <form>
-                <input  type="text" name="title" placeholder="Digite o Título do Curso"/><br></br>
-                <input  type="text" name="descritpion" placeholder="Digite a Descrição do Curso"/><br></br>
-                <input  className="py-4" type="text" name="url" placeholder="Digite a URL do Curso"/><br></br>
-                <button formAction={saveCourses} className="text-[#F56A6A] text-xl text-center bg-white border-2 border-[#F56A6A] px-5 py-[3px] py-8">Salvar</button>
+                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
+                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
+                <input type="text" name="url" placeholder="Digite a url da imagem" /><br/><br/>
+                <button formAction={saveCourse} className="text-white">Salvar</button>
             </form>
         </div>
+
     )
 }
