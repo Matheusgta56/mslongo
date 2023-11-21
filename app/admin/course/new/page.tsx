08026c3be383d4565fd331cc375f7b4fb216bb85
matheusslongo <146895466+Matheusgta56@users.noreply.github.com>
2023-11-21 23:23:38 +0000

    button00

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -14,10 +14,10 @@ export default function NewCourse() {
     return (
         <div>
             <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
-            <form>
-                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
-                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
-                <input type="text" name="url" placeholder="Digite a url da imagem" /><br/><br/>
+            <form >
+                <input className="text-black" type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
+                <input className="text-black"  type="text " name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
+                <input className="text-black"  type="text" name="url" placeholder="Digite a url da imagem" /><br/><br/>
                 <button formAction={saveCourse} className="text-white">Salvar</button>
             </form>
         </div>
